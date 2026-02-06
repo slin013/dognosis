8a2482b3d0896e7b5875007404240cd43f464cca
gregg-v <gregg@vignaux.com>
2026-02-06 12:50:41 -0500

    Implement continuous temperature readings
    Added continuous temperature reading with a 20-second interval.

--- temperature_sensor/instant_temp.py
+++ temperature_sensor/instant_temp.py
@@ -1,6 +1,16 @@
 from temperature_sensor import *
 
+import time
+
 if __name__ == "__main__":
     sensor = MLX90614()
-    print("Object:", sensor.readObjectTemperature())
-    print("Ambient:", sensor.readAmbientTemperature())
+    
+    try:
+        while True:
+            print("Object:", sensor.readObjectTemperature())
+            print("Ambient:", sensor.readAmbientTemperature())
+            print("---")
+            time.sleep(20)  # wait 20 seconds
+    except KeyboardInterrupt:
+        print("Stopped by user")
+
