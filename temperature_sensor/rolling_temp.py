ea710a5cf7243b7d232d350ecb9317f1f9a9913d
Jack-Roche2026 <117380429+Jack-Roche2026@users.noreply.github.com>
2026-02-06 12:38:49 -0500

    Update rolling_temp.py

--- temperature_sensor/rolling_temp.py
+++ temperature_sensor/rolling_temp.py
@@ -1,1 +1,6 @@
+from temperature_sensor import *
 
+if __name__ == "__main__":
+    sensor = MLX90614()
+    print("Object:", sensor.readObjectTemperature())
+    print("Ambient:", sensor.readAmbientTemperature())
